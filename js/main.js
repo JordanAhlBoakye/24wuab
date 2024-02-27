a88c640b614736210f22de2d5d5f76521cdc5fde
JordanAhlBoakye <jordan.ahlboakye@elev.ga.ntig.se>
2024-02-27 13:59:00 +0100

    6thcommit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 document.querySelector(".fadelayer").addEventListener("click",showMenu);
+
+document.querySelector(".menu-button").addEventListener("click",showMenu);
+
+function showMenu(){
+    let menu=document.querySelector("nav.menu");
+    menu.classList.toggle("show");
+    let layer=document.querySelector(".fadelayer");
+    layer.classList.toggle("visible")
+}
