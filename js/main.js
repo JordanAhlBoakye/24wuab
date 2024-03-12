05e8d8e590a394526404c14cb97012622db0d436
JordanAhlBoakye <jordan.ahlboakye@elev.ga.ntig.se>
2024-03-12 14:13:34 +0100

    fixedheader

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.queryselector(".hamburger");
+const navMenu = document.queryselector(".nav-menu");
 
-hamburger.addEventListener("click", =>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
 })
 
-document.querySelectorAll("nav-link").forEach(n=>n.)addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach(n =>n.
+addEventListener("click", ()=>{
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-})
+    
+}))
